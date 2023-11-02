c4c987091028d5aff2c46d4fa6d9ab3113c3fd77
kdonthi <kaushikdr@gmail.com>
2023-11-02 12:48:54 -0700

    Remove RSA auth

--- test/middleware_test.go
+++ test/middleware_test.go
@@ -1,8 +1,6 @@
 package test
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
 	"encoding/json"
 	"github.com/curio-research/keystone/server"
 	ethcrypto "github.com/ethereum/go-ethereum/crypto"
@@ -10,40 +8,6 @@ import (
 	"github.com/stretchr/testify/require"
 	"testing"
 )
-
-func Test_PublicKeyAuth_RSA(t *testing.T) {
-	testSystem := server.CreateSystemFromRequestHandler(func(ctx *server.TransactionCtx[testPersonRequest]) {
-		req := ctx.Req
-		newPerson := req.Data.Val
-
-		personTable.Set(ctx.W, req.Data.Entity, newPerson)
-	}, server.VerifyRSAPublicKeyAuth[testPersonRequest]())
-
-	ctx := initializeTestWorld(testSystem)
-	personTable.AddSpecific(ctx.World, testEntity1, Person{
-		Name: testName1,
-	})
-
-	privateKey, err := rsa.GenerateKey(rand.Reader, 2048)
-	require.Nil(t, err)
-
-	request := testPersonRequest{Val: Person{Name: testName2}, Entity: testEntity1}
-	publicKeyAuth, err := server.NewRSAPublicKeyAuth(privateKey, request)
-	require.Nil(t, err)
-
-	b, err := json.Marshal(publicKeyAuth)
-	require.Nil(t, err)
-
-	keystoneReq := server.NewKeystoneTx(request, map[server.HeaderField]json.RawMessage{
-		server.RSAPublicKeyAuthHeader: b,
-	})
-
-	server.QueueTxFromExternal(ctx, keystoneReq, "")
-	server.TickWorldForward(ctx, 2)
-
-	newPerson := personTable.Get(ctx.World, testEntity1)
-	assert.Equal(t, newPerson.Name, testName2)
-}
 
 func Test_PublicKeyAuth_ECDSA(t *testing.T) {
 	testSystem := server.CreateSystemFromRequestHandler(func(ctx *server.TransactionCtx[testPersonRequest]) {
